52693661c9c752d338ceab32df2cc7e443f4ded6
Vivek Mewada <vmewada205@gmail.com>
2025-11-07 17:34:00 +0530

    Add Employee CRUD endpoints (green)

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,51 @@
+class EmployeesController < ApplicationController
+  before_action :set_employee, only: [:show, :update, :destroy, :salary]
+
+  # Rescue for not found records
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  def index
+    employees = Employee.all
+    render json: employees, status: :ok
+  end
+
+  def show
+    render json: @employee, status: :ok
+  end
+
+  def create
+    employee = Employee.new(employee_params)
+    if employee.save
+      render json: employee, status: :created
+    else
+      render json: { errors: employee.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @employee.update(employee_params)
+      render json: @employee, status: :ok
+    else
+      render json: { errors: @employee.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @employee.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_employee
+    @employee = Employee.find(params[:id])
+  end
+
+  def employee_params
+    params.require(:employee).permit(:full_name, :job_title, :country, :salary)
+  end
+
+  def record_not_found
+    render json: { errors: ['Employee not found'] }, status: :not_found
+  end
+end
